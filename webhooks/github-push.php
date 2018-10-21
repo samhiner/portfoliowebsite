2a3be41759a367e73c2f001df82a5f1399237a77
samhiner <25464398+samhiner@users.noreply.github.com>
2018-10-21 18:11:29 -0400

    Update webhook handler to work with projects on their own git tree.

--- webhooks/github-push.php
+++ webhooks/github-push.php
@@ -11,28 +11,35 @@
 </head>
 <body>
 	<pre>
-	                      .--.
-	                    .'    ',
-	                  .'        ',
-	                 /\/\/\/\/\/\/\
-	                /\/\/\/\/\/\/\/\
-	               |                |
-	               |/\/\/\/\/\/\/\/\|
-	               |\/\/\/\/\/\/\/\/|
-	               |                |
-	                \/\/\/\/\/\/\/\/
-	                 \/\/\/\/\/\/\/
-	                  `.        .'
-	                    `..__..'
+						  .--.
+						.'	',
+					  .'		',
+					 /\/\/\/\/\/\/\
+					/\/\/\/\/\/\/\/\
+				   |				|
+				   |/\/\/\/\/\/\/\/\|
+				   |\/\/\/\/\/\/\/\/|
+				   |				|
+					\/\/\/\/\/\/\/\/
+					 \/\/\/\/\/\/\/
+					  `.		.'
+						`..__..'
 	</pre>
 </body>
 </html>
 <?php
 //ik your not supposed to hardcode your github secret, much less put it online
 //but it just updates the server, so go ahead and update it hackerman.
-$hookSecret = 'pepperoni pepperoni give me the pizzaroni';
+$mainSecret = 'pepperoni pepperoni give me the pizzaroni';
 
-//following script from https://gist.github.com/milo/daed6e958ea534e4eba3.
+function isHash($secret) {
+	global $algo;
+	global $rawPost;
+	global $hash;
+	return $hash === hash_hmac($algo, $rawPost, $secret);
+}
+
+//following script modified from https://gist.github.com/milo/daed6e958ea534e4eba3.
 
 set_error_handler(function($severity, $message, $file, $line) {
 	throw new \ErrorException($message, 0, $severity, $file, $line);
@@ -51,16 +58,28 @@ if (!isset($_SERVER['HTTP_X_HUB_SIGNATURE'])) {
 } elseif (!extension_loaded('hash')) {
 	throw new \Exception("Missing 'hash' extension to check the secret code validity.");
 }
+
 list($algo, $hash) = explode('=', $_SERVER['HTTP_X_HUB_SIGNATURE'], 2) + array('', '');
+
 if (!in_array($algo, hash_algos(), TRUE)) {
 	throw new \Exception("Hash algorithm '$algo' is not supported.");
 }
+
 $rawPost = file_get_contents('php://input');
-if ($hash !== hash_hmac($algo, $rawPost, $hookSecret)) {
-	throw new \Exception('Hook secret does not match.');
-} else {
-	var_dump(shell_exec('sudo ../../shellscripts/serverupdate.sh 2>&1'));
-	echo '<strong>nice job hackerman</strong>';
+
+//TODO turn and thing into a loop through an array
+$done = false;
+foreach (file('../../shellscripts/webhooklist.txt') as $line) {
+	if isHash($line) {
+		var_dump(shell_exec('sudo ../../shellscripts/serverupdate.sh ' . $line . ' 2>&1'));
+		$done = true;
+	}
+}
+
+//TEST making new file by pushing normal code
+
+if (!$done) {
+	throw new \Exception('Hook secret does not match.');	
 }
 
 ?>
